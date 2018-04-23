3524917a61f34416c2e968629848708ed3b75311
Tabitha <tabithahsia@gmail.com>
2018-04-23 02:59:36 -0700

    Updated zoom and centered the US map

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,13 +5,7 @@ class Map extends Component {
   constructor(props){
     super(props);
     this.state = {
-        locations: [
-          { name: "New York County Supreme Court", location: {lat: 40.7143033, lng: -74.0036919} },
-          { name: "Queens County Supreme Court", location: {lat: 40.7046946, lng: -73.8091145} },
-          { name: "Kings County Supreme Court", location: {lat: 40.6940226, lng: -73.9890967} },
-          { name: "Richmond County Supreme Court", location: {lat: 40.6412336, lng: -74.0768597} },
-          { name: "Bronx Supreme Court", location: {lat: 40.8262388, lng: -73.9235238} }
-        ]
+        locations: this.props.locations
     }
   }
 
@@ -31,19 +25,21 @@ class Map extends Component {
       const node = ReactDOM.findDOMNode(mapRef);
 
       const mapConfig = Object.assign({}, {
-        center: {lat: 40.7485722, lng: -74.0068633}, // sets center of google map to NYC.
-        zoom: 11,
+        center: {lat: 41.850033, lng: -87.6500523}, // Chicago - hopefully the center
+        zoom: 3,
         mapTypeId: 'roadmap'
       })
 
       this.map = new maps.Map(node, mapConfig);
-      this.state.locations.forEach( location => { // iterate through locations saved in state
-  const marker = new google.maps.Marker({ // creates a new Google maps Marker object.
-    position: {lat: location.location.lat, lng: location.location.lng}, // sets position of marker to specified location
-    map: this.map, // sets markers to appear on the map we just created on line 35
-    title: location.name // the title of the marker is set to the name of the location
-  });
-})
+      if(this.state.locations.length > 0){
+        this.state.locations.forEach( location => { // iterate through locations saved in state
+          const marker = new google.maps.Marker({ // creates a new Google maps Marker object.
+            position: {lat: location.location.latitude, lng: location.location.longitude}, // sets position of marker to specified location
+            map: this.map, // sets markers to appear on the map we just created on line 35
+            title: location.name // the title of the marker is set to the name of the location
+          });
+        })
+      }
     }
   }
 
