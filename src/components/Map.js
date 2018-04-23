8438d1beabe28b8e05ee20212a697e30d8137b45
Tabitha <tabithahsia@gmail.com>
2018-04-22 23:43:02 -0700

    Added test markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 
 class Map extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        locations: [
+          { name: "New York County Supreme Court", location: {lat: 40.7143033, lng: -74.0036919} },
+          { name: "Queens County Supreme Court", location: {lat: 40.7046946, lng: -73.8091145} },
+          { name: "Kings County Supreme Court", location: {lat: 40.6940226, lng: -73.9890967} },
+          { name: "Richmond County Supreme Court", location: {lat: 40.6412336, lng: -74.0768597} },
+          { name: "Bronx Supreme Court", location: {lat: 40.8262388, lng: -73.9235238} }
+        ]
+    }
+  }
+
+
   componentDidMount() {
+    this.loadMap();
+  }
+  componentDidUpdate() {
     this.loadMap();
   }
 
@@ -20,6 +37,13 @@ class Map extends Component {
       })
 
       this.map = new maps.Map(node, mapConfig);
+      this.state.locations.forEach( location => { // iterate through locations saved in state
+  const marker = new google.maps.Marker({ // creates a new Google maps Marker object.
+    position: {lat: location.location.lat, lng: location.location.lng}, // sets position of marker to specified location
+    map: this.map, // sets markers to appear on the map we just created on line 35
+    title: location.name // the title of the marker is set to the name of the location
+  });
+})
     }
   }
 
