e9a9d3e8218ba440a1fb4ba7856518f34190c615
Tabitha <tabithahsia@gmail.com>
2018-04-23 05:58:09 -0700

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require(`html-webpack-plugin`);
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '..', 'client/'),
+    path: path.join(__dirname, '..', 'dist/'),
     filename: 'bundle.js'
   },
   module: {
