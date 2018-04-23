6f339edf63162a573f4cb03d4d2a494a7dabc446
Tabitha <tabithahsia@gmail.com>
2018-04-23 02:53:55 -0700

    Finished getLocations route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,25 @@ var port = 8080;
 
 app.use(express.static(path.join(__dirname, '..', 'dist/')));
 
+app.get('/', function(req, res){
+  // data = db.data;
+})
+
+
+app.get('/getLocations/:disaster/:year', function(req, res){
+  console.log('req', req.params);
+  let locations = [];
+  let data = db.data;
+  // console.log('this is the data finally loaded! ', Array.isArray(data));
+  for(var i = 0; i < data.length; i++){
+    if((data[i].year === Number(req.params.year)) && (data[i].disaster === req.params.disaster)){
+      locations.push([data[i].latitude, data[i].longitude]);
+    }
+  }
+  console.log(locations);
+
+  res.send(locations);
+})
 
 app.listen(port, function(){
   console.log('Listening on port 8080');
